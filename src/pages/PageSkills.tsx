a5781ffdaf3a65d7f8848620778c86909cbcc1eb
AbdulSamman <sammanab@outlook.de>
2024-05-22 23:44:06 +0200

    skills finished

--- src/pages/PageSkills.tsx
+++ src/pages/PageSkills.tsx
@@ -5,11 +5,16 @@ export const PageSkills = () => {
   const { rawSkills } = useContext(AppContext);
   return (
     <div className="pageSkills">
-      <p>welcome to the Skills page</p>
       <div className="skills">
         {Object.keys(rawSkills).map((idCode) => (
           <div className="skill" key={idCode}>
             <h3 className="skillName">{rawSkills[idCode].name}</h3>
+            <p className="description">{rawSkills[idCode].description}</p>
+            <p className="skillUrl">
+              <a href={rawSkills[idCode].url} target="_blank">
+                {rawSkills[idCode].url}
+              </a>
+            </p>
           </div>
         ))}
       </div>
